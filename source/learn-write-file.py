96a0989565f96b9f74ebca28369e5a46b92aa611
pichappan <opichappan@gmail.com>
2020-09-09 23:38:40 -0400

    web
    spyder

--- source/learn-write-file.py
+++
@@ -1,9 +0,0 @@
-# w - will over write
-# a will append at end
-#r+ for both read and write
-
-
-employee_file = open("employee.txt","r+")
-employee_file.write("Kelly- Couster Service\n") #excape char
-print(employee_file.read())
-employee_file.close()
